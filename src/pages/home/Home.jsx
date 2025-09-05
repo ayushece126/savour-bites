5f15951894a4d7f07359aae01457d4bfe95dd89b
ayushece126 <mayush9652@gmail.com>
2025-09-05 22:46:11 +0530

    fixed home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,59 +5,59 @@ import { useLocation } from "../../hooks/useLocation";
 import { Link, useNavigate } from "react-router-dom";
 
 export default function Home() {
-    const { fetchCurrentLocation, addDemoCoordinate } = useLocation();
+  const { fetchCurrentLocation, addDemoCoordinate } = useLocation();
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div className="home">
-            <div className="home-logo">SavourBite</div>
-            <div className="home-body">
-                <div className="home-details">
-                    <div className="home-text">
-                        <span>
-                            Your <span>Cravings</span>,
-                        </span>
-                        <span>Delivered at the speed of a click. </span>
-                        <span>
-                            The quickest way to find your <span>food</span>
-                        </span>
-                    </div>
+  return (
+    <div className="home">
+      <div className="home-logo">SavourBite</div>
+      <div className="home-body">
+        <div className="home-details">
+          <div className="home-text">
+            <span>
+              Your <span>Cravings</span>,
+            </span>
+            <span>Delivered at the speed of a click. </span>
+            <span>
+              The quickest way to find your <span>food</span>
+            </span>
+          </div>
 
-                    <div className="btn-container">
-                        <button
-                            onClick={async () => {
-                                await fetchCurrentLocation();
-                                navigate("/restaurant");
-                            }}
-                            className="primary-btn location-btn search-size"
-                        >
-                            <LocateFixed />
-                            Location
-                        </button>
+          <div className="btn-container">
+            <button
+              onClick={async () => {
+                await fetchCurrentLocation();
+                navigate("/restaurant");
+              }}
+              className="primary-btn location-btn search-size"
+            >
+              <LocateFixed />
+              Location
+            </button>
 
-                        <button
-                            onClick={async () => {
-                                await addDemoCoordinate();
-                                navigate("/restaurant");
-                            }}
-                            className="primary-btn search-size"
-                        >
-                            <LocateOffIcon />
-                            Demo
-                        </button>
-                    </div>
-                </div>
-                <div className="home-illustration">
-                    <img src={homeIllustration} alt="" loading="lazy" />
-                </div>
-            </div>
-
-            <div className="home-footer">
-                <Link to={"https://github.com/Harsh12Codes"} target="_blank">
-                    Created🧡@Harsh12codes
-                </Link>
-            </div>
+            <button
+              onClick={async () => {
+                await addDemoCoordinate();
+                navigate("/restaurant");
+              }}
+              className="primary-btn search-size"
+            >
+              <LocateOffIcon />
+              Demo
+            </button>
+          </div>
         </div>
-    );
+        <div className="home-illustration">
+          <img src={homeIllustration} alt="" loading="lazy" />
+        </div>
+      </div>
+
+      <div className="home-footer">
+        <Link to={"github.com/anjalimehta0310"} target="_blank">
+          Created🧡@Anjali
+        </Link>
+      </div>
+    </div>
+  );
 }
